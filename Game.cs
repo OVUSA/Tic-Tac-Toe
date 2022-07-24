cca8776409f52cd7e25b74f15c2067bee556e53b
OVUSA <ovelichko@luc.edu>
2022-07-23 23:10:29 -0500

    added first class

---
+++ Game.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Logic{
+    public class Game{
+        string player1="Joe";
+        string player = "Mary";
+
+        public void players(){
+            Console.Write($"Players are {player1} and {player}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Logic;
+
+namespace Program{
+    public class Program{
+
+        public static void Main(string [] args){
+            var game = new Game();
+            game.players();
+
+
+        }
+    }
+}
