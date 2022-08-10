8cedbe732e6098e342c7da482bce099f7224cbd9
OVUSA <ovelichko@luc.edu>
2022-08-09 21:17:40 -0500

    added level of difficulty

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ namespace Program{
     public class Program{
 
         public static void Main(string [] args){
-            Console.WriteLine("Welcome to Tic-Tac-Toe game.You're gonna play with a computer.");
-            Console.Write("Select the symbol to play X/O ?");
+            Console.WriteLine("Welcome to Tic-Tac-Toe game.You're gonna play with a computer"+
+            "You can choose level of difficulty 'e/d' which is easy/difficult.");
+            string difficultyLevel = Console.ReadLine();
+
+            Console.Write("Select a symbol to play with :  X/O ?");
             Console.WriteLine();
-           string userSymbol = Console.ReadLine();
-            var game = new Game(userSymbol);
+            string userSymbol = Console.ReadLine();
+            var game = new Game(userSymbol,difficultyLevel);
         }
     }
 }
