689669a99132c06fdf9795a2eb2580e410367530
OVUSA <ovelichko@luc.edu>
2022-09-13 17:27:37 -0500

    updating logic

---
+++ BoardLogic/Board.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLogic
+{
+    public class Board
+    {
+        public int[] Grid = new int[9];
+
+        public Board()
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                Grid[i] = 0;
+            }
+        }
+
+        //TODO
+        public int checkWinner()
+        {
+            if (Grid[0] == Grid[1] && Grid[1] == Grid[2] && Grid[1] != 0)
+            {
+                return Grid[0];
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        public bool isBoardFull()
+        {
+            bool isFull = true;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Grid[i] == 0)
+                {
+                    return false;
+                }
+             }
+            return true;
+        }
+
+
+    }
+
+
+    
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using BoardLogic;
+
+namespace Program
+{
+    public class TicTacToe
+    {
+        static Board game = new Board();
+        static  Boolean winner = true;
+
+        public static void Main(string[] args)
+        {
+            //fill up the board
+
+            printBoard();
+            Console.WriteLine("Plase enter a number from 0 to 8");
+            int userTurn = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your input is: "+ userTurn);
+
+            while (game.checkWinner() == 0)
+            {
+
+            }
+            
+
+
+        }
+
+        // Player 1 == X ; Player 2 = O
+        public static void printBoard()
+        {
+            for( int i = 0; i < game.Grid.Length; i++)
+            {
+                if (game.Grid[i] == 0)
+                {
+                    Console.Write(" . ");
+                }else if (game.Grid[i] == 1)
+                {
+                    Console.Write(" X ");
+                }else if (game.Grid[i] == 2)
+                {
+                    Console.Write(" O ");
+                }
+
+                if(i==2 || i == 5)
+                {
+                    Console.WriteLine();
+
+                }
+            }
+        }
+
+        public void validateMove()
+        {
+
+        }
+    }
+}
